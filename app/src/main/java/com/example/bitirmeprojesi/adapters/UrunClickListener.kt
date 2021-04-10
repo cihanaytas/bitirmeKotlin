6ccc754dd16c9c8ac80d017f89dad953d583aead
cihanaytas <cihan.aytas.6169@gmail.com>
2021-04-10 19:58:10 +0300

    product update

---
+++ app/src/main/java/com/example/bitirmeprojesi/adapters/UrunClickListener.kt
@@ -0,0 +1,4 @@
+package com.example.bitirmeprojesi.adapters
+
+interface UrunClickListener {
+}
---
+++ app/src/main/java/com/example/bitirmeprojesi/viewmodel/CustomerUrunlerViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.bitirmeprojesi.view
+
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.bitirmeprojesi.activities.serviceCustomer
+import com.example.bitirmeprojesi.methods.CustomerWorkFlow
+import com.example.bitirmeprojesi.models.products.Product
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class CustomerUrunlerViewModel(application: Application) :  ViewModel(){
+
+    val urunler = MutableLiveData<List<Product>>()
+
+
+    fun urunleriAl(){
+        val wf = CustomerWorkFlow(serviceCustomer)
+        GlobalScope.launch(Dispatchers.Main) {
+            val a = wf.getProductList()
+            urunler.value = a
+        }
+
+    }
+
+
+
+}
